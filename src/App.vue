d6199a257f16905952b798dc649ebd643a26ef23
songyingjie <yingjiesong218828@sohu-inc.com>
2023-05-27 14:12:44 +0800

    feat: 🎸 add critical code

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,83 @@ export default {
 }
 </script>
 
-<style>
-/*每个页面公共css */
+<style lang="scss">
+.amap-info-window__low {
+  min-width: 180rpx;
+  padding: 8rpx 20rpx;
+  border: 1rpx solid #fbb60d;
+  border-radius: 10rpx;
+  background: #fff;
+  box-shadow: 0px 8px 17rpx 0px rgba(189, 193, 215, 0.5);
+  position: relative;
+  text-align: center;
+  &::before {
+    content: "";
+    display: block;
+    border-top: 11rpx solid #fbb60d;
+    border-left: 11rpx solid transparent;
+    border-right: 11rpx solid transparent;
+    border-bottom: 11rpx solid transparent;
+    position: absolute;
+    top: calc(100% + 11rpx);
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  &::after {
+    content: "";
+    display: block;
+    border-top: 10rpx solid #fff;
+    border-left: 10rpx solid transparent;
+    border-right: 10rpx solid transparent;
+    border-bottom: 10rpx solid transparent;
+    position: absolute;
+    top: calc(100% + 10rpx);
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .amap-info-title {
+    font-size: 22rpx;
+    font-weight: 800;
+    color: #303033;
+    line-height: 30rpx;
+  }
+  .amap-info-price {
+    margin-top: 4rpx;
+    font-size: 22rpx;
+    font-weight: 800;
+    color: #6d6d73;
+    line-height: 30rpx;
+  }
+}
+.amap-info-window__normal {
+  @extend .amap-info-window__low;
+  border: 1rpx solid #fb6b52;
+  &::before {
+    border-top: 11rpx solid #fb6b52;
+  }
+}
+.amap-info-window__high {
+  @extend .amap-info-window__low;
+  border: 1rpx solid #ff4444;
+  &::before {
+    border-top: 11rpx solid #ff4444;
+  }
+}
+.amap-info-window__selected {
+  @extend .amap-info-window__low;
+  border: 1rpx solid #ff4444;
+  background: #ff4444;
+  &::before {
+    border-top: 11rpx solid #ff4444;
+  }
+  &::after {
+    border-top: 11rpx solid #ff4444;
+  }
+  .amap-info-title {
+    color: #ffffff;
+  }
+  .amap-info-price {
+    color: #ffffff;
+  }
+}
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,48 +1,239 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+  <div class="container">
+    <div id="map"></div>
+  </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
+<script setup>
+import AMapLoader from "@amap/amap-jsapi-loader";
+import HouseListJson from "../../db/data.json";
+import { ref } from "vue";
+
+// data
+let map = null; // 地图母版
+let zoom = null; // 地图缩放等级
+let center = null; // 地图中心点坐标
+let geocoder = null; // 地图编码转换器
+let massMarks = null; // 海量点标记
+let layer = null; // 气泡层
+let markers = null; // 气泡层圆点标记
+let labels = []; // 气泡层海量标注
+let selectedLabel = []; // 气泡层选中态到文本标注
+
+const priceIconMap = {
+  low: "https://t1.focus-img.cn/applet/2022-04-25/a7830c321d0842dea1ce772f622fce02.png",
+  normal:
+    "https://t1.focus-img.cn/applet/2022-04-25/721f4aee79ed4a3299fee8692b51c81c.png",
+  high: "https://t1.focus-img.cn/applet/2022-04-25/37df30680cce4b41b361a6b681edb9b0.png",
+};
+const priceDotArr = [
+  "https://t1.focus-img.cn/applet/2022-04-25/fb5c5879cec340af9ee9ee7453abbff6.png", // low
+  "https://t1.focus-img.cn/applet/2022-04-25/f0f352851311438ebf69b130369d9e64.png", // normal
+  "https://t1.focus-img.cn/applet/2022-04-25/b64526f72e59426f95922b19b2043a3b.png", // high
+  "https://t1.focus-img.cn/applet/2022-04-14/db7245505a904ceba96fb92efb9157a0.jpg", // active
+];
+const priceClassArr = ["low", "normal", "high"];
+const cityName = ref("深圳");
+const houseList = ref([]);
+const screenHouseList = ref([]);
+
+// methods
+const getCityLngLat = (cityId) => {
+  AMapLoader.load({
+    key: "", // 申请好的Web端开发者Key，首次调用 load 时必填
+    version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+    plugins: ["AMap.Geocoder"], // 需要使用的的插件列表，如比例尺'AMap.Scale'、'AMap.CitySearch'等
+  })
+    .then((AMap) => {
+      // 准备地图编码器 geocoder
+      geocoder = new AMap.Geocoder({
+        city: cityId, // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
+      });
+      geocoder.getLocation(cityName.value, function (status, result) {
+        if (status === "complete" && result.info === "OK") {
+          let location = [
+            result.geocodes[0].location.lng,
+            result.geocodes[0].location.lat,
+          ];
+          map = new AMap.Map("map", {
+            resizeEnable: true, //是否监控地图容器尺寸变化
+            zoom: 10, //初始化地图层级
+            center: location, //初始化地图中心点
+          }); // 展示地图 map
+
+          setMassMarks(); // 设置海量点标记 marker
+          setMapListener(); // 监听地图 缩放/移动 事件
+        }
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+const setMassMarks = () => {
+  const lowPriceIconStyle = {
+    url: priceIconMap.low,
+    size: new AMap.Size(30, 30),
+    anchor: "center",
+  };
+  const normalIconStyle = {
+    url: priceIconMap.normal,
+    size: new AMap.Size(30, 30),
+    anchor: "center",
+  };
+  const highPriceIconStyle = {
+    url: priceIconMap.high,
+    size: new AMap.Size(30, 30),
+    anchor: "center",
+  };
+  const styleObjectArr = [
+    lowPriceIconStyle,
+    normalIconStyle,
+    highPriceIconStyle,
+  ];
+  const markerList = houseList.value.map((item) => ({
+    lnglat: item.location.split(","),
+    name: item.projName,
+    id: item.id,
+    style: item.level - 1,
+  }));
+  massMarks = new AMap.MassMarks(markerList, {
+    zIndex: 500, // 海量点图层叠加的顺序
+    zooms: [3, 14], // 在指定地图缩放级别范围内展示海量点图层
+    style: styleObjectArr, // 设置样式数组
+  });
+  massMarks.setMap(map);
+};
+const setMapListener = () => {
+  map.on("zoomend", () => {
+    // 监听地图缩放结束后的等级
+    zoom = map.getZoom();
+    if (zoom >= 14) {
+      executeConditionRender();
     }
-  },
-  onLoad() {},
-  methods: {},
-}
+  });
+  map.on("moveend", () => {
+    // 监听地图中心点的位置变化
+    center = map.getCenter();
+    if (zoom >= 14) {
+      executeConditionRender();
+    }
+  });
+};
+const executeConditionRender = () => {
+  let screenCoordinateRange = map.getBounds();
+  let northEast = [
+    screenCoordinateRange.northEast.lng,
+    screenCoordinateRange.northEast.lat,
+  ];
+  let southEast = [
+    screenCoordinateRange.southWest.lng,
+    screenCoordinateRange.northEast.lat,
+  ];
+  let southWest = [
+    screenCoordinateRange.southWest.lng,
+    screenCoordinateRange.southWest.lat,
+  ];
+  let northWest = [
+    screenCoordinateRange.northEast.lng,
+    screenCoordinateRange.southWest.lat,
+  ];
+
+  screenHouseList.value = houseList.value.filter((item) => {
+    return AMap.GeometryUtil.isPointInRing(item.location.split(","), [
+      northEast,
+      southEast,
+      southWest,
+      northWest,
+    ]);
+  });
+  map.removeLayer(layer); // 删除原来气泡层（包含圆点标记）
+  map.remove(labels); // 删除原来气泡层的海量标注
+  setLabelsLayer(); // 设置海量标注
+};
+const setLabelsLayer = () => {
+  layer = new AMap.LabelsLayer({
+    zooms: [14, 20],
+    allowCollision: true, // 让标注避让用户的标注
+  });
+  markers = [];
+  labels = [];
+  screenHouseList.value.map((item) => {
+    // 圆点图标识部分 ⬇️
+    const labelMarker = new AMap.LabelMarker({
+      icon: {
+        type: "image",
+        size: [12, 12],
+        anchor: "center",
+        image: priceDotArr[item.level - 1],
+      },
+      zooms: [14, 20],
+      position: item.location.split(","),
+    });
+    markers.push(labelMarker);
+
+    // 气泡文案部分 ⬇️
+    const normalMarker = new AMap.Marker({
+      zooms: [14, 20],
+      offset: new AMap.Pixel(0, -15),
+      extData: item,
+    });
+    normalMarker.setContent(
+      `<div class="amap-info-window__${priceClassArr[item.level - 1]}">
+              <div class="amap-info-title">${item.projName}</div>
+              <div class="amap-info-price">${item.price}元/平方米</div>
+          </div>`
+    );
+    normalMarker.setPosition(item.location.split(","));
+    normalMarker.on("click", function (e) {
+      setNormalMarkerSelected(e.target.getExtData().id);
+    });
+    labels.push(normalMarker);
+  });
+  layer.add(markers);
+  map.add(layer);
+  map.add(labels);
+};
+const setNormalMarkerSelected = (selectedId) => {
+  map.remove(selectedLabel); // 删除原来的 selectedLabel 标注
+  selectedLabel = [];
+  screenHouseList.value.map((item) => {
+    if (item.id !== selectedId) return;
+
+    // 气泡文案部分 ⬇️
+    const normalMarker = new AMap.Marker({
+      zooms: [14, 20],
+      offset: new AMap.Pixel(0, -15),
+      extData: item,
+    });
+    normalMarker.setContent(
+      `<div class="amap-info-window__selected">
+              <div class="amap-info-title">${item.projName}</div>
+              <div class="amap-info-price">${item.price}元/平方米</div>
+          </div>`
+    );
+    normalMarker.setPosition(item.location.split(","));
+    normalMarker.on("click", function (e) {
+      setNormalMarkerSelected(e.target.getExtData().id);
+    });
+    selectedLabel.push(normalMarker);
+  });
+  map.add(selectedLabel);
+};
+
+// created
+houseList.value = HouseListJson;
+getCityLngLat(197);
 </script>
 
-<style>
-.content {
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  #map {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
