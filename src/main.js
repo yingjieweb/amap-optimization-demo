1731e7d3315b39b7475b7f28c5a63b5f8c84686c
yingjieweb <1184061078@qq.com>
2023-05-30 20:35:09 +0800

    feat: 🎸 install vant and sass

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import {
-	createSSRApp
-} from "vue";
+import { createSSRApp } from "vue";
 import App from "./App.vue";
+import { Dialog, Field } from "vant";
+
 export function createApp() {
-	const app = createSSRApp(App);
-	return {
-		app,
-	};
+  const app = createSSRApp(App);
+  app.use(Dialog);
+  app.use(Field);
+
+  return {
+    app,
+  };
 }
