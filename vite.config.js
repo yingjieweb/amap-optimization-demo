6a2286e7e82e54fa1006f31addd1a79d2a24c886
yingjieweb <1184061078@qq.com>
2023-05-30 20:49:37 +0800

    feat: 🎸 add publicPath

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
   ],
+  base: '/amap-optimization-demo/'
 })
