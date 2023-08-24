fe598db889cb785e04105423c73b0f2dc0226259
yingjieweb <1184061078@qq.com>
2023-08-24 11:16:35 +0800

    feat: 🎸 Update AMapDeveloperKey

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,4 @@
-export const AMapDeveloperKey = "";
+// 🚨 此 Key 仅用于该 Demo 项目演示使用，随时可能被暂停
+// 🚨 如需开发正式的地图相关业务，请自行前往高德开放平台申请
+// 🚨 https://lbs.amap.com/api/javascript-api-v2/prerequisites
+export const AMapDeveloperKey = "8e342d7d66ccb8ec1b251540d9f2efc2";
